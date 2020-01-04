761ce7fed388948960312faf4b361a3a42bb6131
Brandon Makin <bmicmak@gmail.com>
2020-01-03 22:20:30 -0800

    movement works on bash boiiii

--- floorDisplay.py
+++ floorDisplay.py
@@ -5,41 +5,41 @@ Created on Fri Jan  3 15:58:50 2020
 @author: Kam Look
 """
 import numpy as np
-import msvcrt as microsoft
 import display as dis
+import cliInput
 
-def getInput():
-    nextInput = microsoft.getwch()
-    if nextInput in "aA":
-        return 'left'
-    if nextInput in "dD":
-        return 'right'
-    if nextInput in "wW":
-        return 'up'
-    if nextInput in "sS":
-        return 'down'
-    if nextInput in "qQ":
-        return 'quit'
+# def getInput():
+#     nextInput = microsoft.getwch()
+#     if nextInput in "aA":
+#         return 'left'
+#     if nextInput in "dD":
+#         return 'right'
+#     if nextInput in "wW":
+#         return 'up'
+#     if nextInput in "sS":
+#         return 'down'
+#     if nextInput in "qQ":
+#         return 'quit'
 
 def updatePlayerPos():
     entityViewport.data[Player.ypos][Player.xpos]=Player.icon
     dis.render()
-    
+
 #Tile list
 class Player:
     icon='@'
     xpos=0
     ypos=0
-    
-def split(word): 
-    return [char for char in word]  
 
-#read contents of file and put into a list 
+def split(word):
+    return [char for char in word]
+
+#read contents of file and put into a list
 file=open('floor1.txt', 'r')
 floorDisplay=file.readlines().copy()
 file.close()
 
-#remove white space 
+#remove white space
 
 floorDisplay[:] = [row.strip('\n') for row in floorDisplay]
 floorDisplay[:] = [row.strip('\t') for row in floorDisplay]
@@ -53,28 +53,26 @@ entityViewport=dis.addViewport(0,0,entityArray, z=-1)
 updatePlayerPos()
 
 while True:
-    move=getInput()
+    move=cliInput.getInput()
     if move =='up':
         entityViewport.data[Player.ypos][Player.xpos] = ' '
         Player.ypos -= 1
         updatePlayerPos()
-        
+
     if move == 'down':
         entityViewport.data[Player.ypos][Player.xpos] = ' '
         Player.ypos += 1
         updatePlayerPos()
-        
+
     if move == 'right':
         entityViewport.data[Player.ypos][Player.xpos] = ' '
         Player.xpos += 1
         updatePlayerPos()
-        
+
     if move == 'left':
         entityViewport.data[Player.ypos][Player.xpos] = ' '
         Player.xpos -= 1
         updatePlayerPos()
-    
+
     if move == 'quit':
         exit(0)
-
-
