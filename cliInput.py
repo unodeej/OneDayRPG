8bd5575aec8f3ef8226b0ed9a3d02309be688739
Brandon Makin <bmicmak@gmail.com>
2020-01-03 21:07:44 -0800

    cliInput doesn't mess up command line for linux users

--- cliInput.py
+++ cliInput.py
@@ -3,13 +3,16 @@ import os
 if (os.name == 'posix'):
     import tty
     import sys
+    import termios
 else: # current os is windows
     import msvcrt as microsoft
 
 def getInput():
     if (os.name == 'posix'):
+        originalCommandLineSettings = termios.tcgetattr(sys.stdin)
         tty.setcbreak(sys.stdin)
         nextInput = sys.stdin.read(1)[0]
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, originalCommandLineSettings)
     else: # current os is windows
         nextInput = microsoft.getwch()
     if nextInput in "aA":
