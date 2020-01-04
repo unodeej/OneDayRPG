ef4f1df1bb3978500f2f9193ffbcfaac6b1dfa2c
Brandon Makin <bmicmak@gmail.com>
2020-01-04 00:28:39 -0800

    added popups

---
+++ popup.py
@@ -0,0 +1,58 @@
+import display
+import numpy
+
+class Popup:
+    x = display.displayWidth//4
+    y = display.displayHeight//4
+    width = display.displayWidth//2
+    border = 2
+    textPerLine = width - 2*border
+
+    def __init__(self, text, buttons, centeredOnScreen=True, x=0, y=0):
+        self.text = text
+        self.buttons = buttons
+        borderSymbol = "░"
+
+        if not centeredOnScreen:
+            self.x = x
+            self.y = y
+
+        self.data = []
+        self.data.append([borderSymbol] * self.width) # first line is a border
+        for line in self.splitTextIntoLines():
+            nextLine = [borderSymbol, " "]
+            nextLine += list(line)
+            nextLine += [' '] * (self.width - len(nextLine) - 1) + [borderSymbol] # pad the right of the line
+            self.data.append(nextLine)
+
+        self.data.append([borderSymbol] + [' '] * (self.width - 2) + [borderSymbol])
+        for button in buttons:
+            nextLine = [borderSymbol, " ", ">", ">", " ", "[", button.hotkey ,"]", " ", ]
+            nextLine += button.text
+            nextLine += [' '] * (self.width - len(nextLine) - 1) + [borderSymbol] # pad the right of the line
+            self.data.append(nextLine)
+
+        self.data.append([borderSymbol] * self.width) # last line is a border
+
+
+
+    def show(self):
+        self.viewport = display.addViewport(self.x, self.y, p.data)
+
+    def hide(self):
+        if self.viewport is not None:
+            display.removeViewport(self.viewport)
+
+    def splitTextIntoLines(self):
+        for i in range(0, len(self.text), self.textPerLine):
+            yield self.text[i:i + self.textPerLine]
+
+class Button():
+    def __init__(self, text, hotkey):
+        self.text = text
+        self.hotkey = hotkey
+
+p = Popup(
+    text="There once was a man from Kentucky. With cars, he was not very lucky.  When his truck broke down, the mechanic skipped town, for he and the man's wife did fuck(y)",
+    buttons = [Button(text="okay", hotkey="o"), Button(text="whatever", hotkey="w")]
+)
